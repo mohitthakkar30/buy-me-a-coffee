3ac5fdd48aa30e788ad06106da5e3771fb15fb33
Mohit Thakkar <thakkarmohit90@gmail.com>
2022-10-12 17:20:40 +0530

    First Commit

---
+++ contracts/FundMe.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract FundMe{
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+    address public immutable i_owner;
+
+    constructor(){
+        i_owner = msg.sender;
+    }
+
+    modifier onlyOwner{
+        require(msg.sender == i_owner,"You can't execute this funtion as you are not owner.");
+        _;
+    }
+
+    function addFund() public payable{
+        require(msg.value >= 1e18 ,"Send value >= 1");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
+    }
+
+    function withdraw() public onlyOwner{
+        bool sendSuccess = payable (msg.sender).send(address(this).balance);
+        require(sendSuccess,"Send Failed...");
+    }
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,10 +8,11 @@ import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, provider, webSocketProvider } = configureChains(
   [
-    chain.mainnet,
+    // chain.mainnet,
     chain.polygon,
-    chain.optimism,
-    chain.arbitrum,
+    chain.polygonMumbai,
+    // chain.optimism,
+    // chain.arbitrum,
     ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true'
       ? [chain.goerli, chain.kovan, chain.rinkeby, chain.ropsten]
       : []),
@@ -20,7 +21,7 @@ const { chains, provider, webSocketProvider } = configureChains(
     alchemyProvider({
       // This is Alchemy's default API key.
       // You can get your own at https://dashboard.alchemyapi.io
-      apiKey: '_gg7wSSi0KMBsdKnGVfHDueq6xMB9EkC',
+      apiKey: 'r1xrnkXFSD92Fd715SqGkluuVwQ_5-YK',
     }),
     publicProvider(),
   ]
